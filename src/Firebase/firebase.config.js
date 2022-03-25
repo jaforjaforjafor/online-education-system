2feaca2e62f139ef511c3a60b8d51d13d230d383
jaforjaforjafor <jaforiqbalbd1996@gmail.com>
2022-03-25 13:20:43 +0600

    components-jumma

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZNf98UPd4_k4uSlQle7GivsLln3dv6cM",
+    authDomain: "online-education-system-c4063.firebaseapp.com",
+    projectId: "online-education-system-c4063",
+    storageBucket: "online-education-system-c4063.appspot.com",
+    messagingSenderId: "191030505203",
+    appId: "1:191030505203:web:d6c127990951941ad6d186"
+  };
+  export default firebaseConfig;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider,onAuthStateChanged,signOut } from "firebase/auth";
+import initializeAuthentication from "../Firebase/firebase.init";
+
+initializeAuthentication()
+const useFirebase =()=>{
+    const [user,setUser]=useState({});
+    const [isLoading,setIsLoading]=useState(true);
+
+    const auth=getAuth();
+
+    
+    const signInUsingGoogle =()=>{
+        setIsLoading(true);
+        const googleProvider =new GoogleAuthProvider();
+        signInWithPopup(auth,googleProvider)
+        .then(result =>{
+            setUser(result.user);
+        })
+        .finally(()=>setIsLoading(false));
+       
+    }
+   
+    //observe whether user auth state changed or not
+    useEffect( () =>{
+        onAuthStateChanged(auth,(user) =>{
+            if(user){
+                setUser(user);
+            }
+            else{
+                setUser({})
+            }
+            setIsLoading(false);
+        });
+    },[])
+    const logOut=()=>{
+        setIsLoading(true);
+        signOut(auth)
+        .then(() =>{ })
+        .finally(()=>setIsLoading(false));
+        
+    }
+    return{
+        user,
+        isLoading,
+        signInUsingGoogle,
+        logOut
+    }
+}
+export default useFirebase;
