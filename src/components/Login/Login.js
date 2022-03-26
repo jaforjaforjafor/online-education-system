be3cafacd4cd1e392c04024f217a60493a79d9fe
jaforjaforjafor <jaforiqbalbd1996@gmail.com>
2022-03-26 16:46:12 +0600

    login-try

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -6,25 +7,69 @@ import useAuth from '../../hooks/useAuth';
 import "./Login.css";
 
 const Login = () => {
-    const{signInUsingGoogle}=useAuth();
+    const [loginData,setLoginData]=useState({});
+    const [error,setError]=useState({});
+    const auth = getAuth();
+    const { user, loginUser, isLoading, signInUsingGoogle, authError } = useAuth();
+    
+    
+   
     const location=useLocation();
     const history =useHistory();
     const redirect_url=location.state?.from ||'/';
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+    const handleLoginSubmit = e => {
+        loginUser(loginData.email, loginData.password, location, history);
+        e.preventDefault();
+    }
+    
     const handleGoogleLogin=()=>{
-        signInUsingGoogle()
+        signInUsingGoogle(location,history)
         .then(result =>{
             history.push(redirect_url);
         })
+       
+       
+
+
+const processLogin=(email,password)=>{
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result =>{
+        const user =result.user;
+        console.log(user);
+    })
+    .catch((error) => {
+    setError( error.message);
+      });
+}
+
+  
+  
+  
     }
     return (
         <div className='login-form'>
+            
             <div >
             <h2 className='login-please'>Please Login</h2>
-            <form onSubmit={handleGoogleLogin}>
-                <input type="email" name="" id="" placeholder='Enter Your Email ' /><br /><br />
+            <form onSubmit={handleLoginSubmit}>
+
+                <input onBlur={handleOnChange} type="email" name="" id="" placeholder='Enter Your Email ' /><br /><br />
                 <input type="password" name="" id="" placeholder='Enter Your password ' />
                 <br /><br />
-                <input className="submit-login" type="submit" value="submit" />
+                <input  className="submit-login" type="submit" value="submit" />
+                {user?.email && <div className="alert alert-success" role="alert">
+                    User created successFully!
+                </div>}
+                {authError && <div className="alert alert-danger" role="alert">
+                    {authError}
+                </div>}
             </form>
             <p>New user ? <Link to="/register">create account</Link></p>
             <div>----------Or---------</div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import { getAuth ,createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [error,setError]=useState('');
+
+    const auth = getAuth();
+    const handleEmailChange= e =>{
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange = e =>{
+        setPassword(e.target.value);
+    }
+    const handleregistration= e =>{
+        e.preventDefault();
+        console.log(email, password);
+        if(password.length <6){
+            setError('password must be at least 6 characters long');
+            return;
+        }
+        if(!/(?:[^A-Z]*[A-Z])/.test(password)){
+            setError('password must contain 2 upper case');
+            return;
+        }
+        createUserWithEmailAndPassword(auth,email,password)
+        .then(result =>{
+            const user =result.user;
+            console.log(user);
+            setError('');
+        })
+        .catch(error =>{
+            setError(error.message);
+        })
+       
+
+    }
     return (
-        <div className='rgs-color'>
+        <div className='rgs-color mx-5'>
             <div>
+                
             <h2 >Register:Create Account</h2>
-            <form onSubmit="">
-                <input type="text" name="" id="" placeholder='Enter your first name' /><br /><br />
-                <input type="text" name="" id="" placeholder='Enter your last name' /><br /><br />
-                <input type="email" name="" id="" placeholder='Enter your Email' /><br /><br />
-                <input type="password" name="" id="" placeholder='Enter your Password' /><br /><br />
-                <input type="password" name="" id="" placeholder='Confirm your password' /><br /><br />
-                <input type="number" name="" id="" placeholder='Phone Number' /><br /><br />
+            <form onSubmit={handleregistration}>
+                <input type="text" name="" id="" placeholder='Enter your first name' required /><br /><br />
+                <input type="text" name="" id="" placeholder='Enter your last name' required/><br /><br />
+                <input onBlur={handleEmailChange} type="email" name="" id="" placeholder='Enter your Email' required /><br /><br />
+                <input type="password" name="" id="" placeholder='Enter your Password' required/><br /><br />
+                <input onBlur={handlePasswordChange} type="password" name="" id="" placeholder='Confirm your password' required  />
+                <br /><br />
+                <input type="number" name="" id="" placeholder='Phone Number' required /><br /><br />
+                <div className='text-danger'>{error}</div>
                 <input  className='submit' type="submit" value="submit" /><br /><br />
             </form>
-            <p>Already have an account? <Link to ="/login"></Link> </p>
+            <p>Already have an account? <Link to ="/login">please login</Link> </p>
             <div>-------OR--------</div>
             <button className='btn-regular'>Google Sign In</button>
             </div>
